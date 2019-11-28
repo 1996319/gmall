c3cb3a58246432007ccb233cc7c69c935ca81960
1996319 <1562350788@qq.com>
2019-11-28 11:20:26 +0800

    加入用户管理模块

---
+++ gmall-user-gmall/src/main/java/com/zr/gmall/gmallusergmall/GmallUserGmallApplication.java
@@ -0,0 +1,13 @@
+package com.zr.gmall.gmallusergmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallUserGmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallUserGmallApplication.class, args);
+    }
+
+}
